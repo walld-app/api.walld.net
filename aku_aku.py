a44fd4704f7811f050d1838e071913ff14d58f40
kz159 <loh@kz159.ru>
2019-08-05 23:40:56 +0300

    Setting up base server which will give random walls

---
+++ aku_aku.py
@@ -0,0 +1,30 @@
+#must run by cron every hour to update db with wallpapers that sits in folders
+import os, sqlite3
+SEARCH_DIR = '/mnt/ntfs-drive/walld_pics'
+DB_FILE = SEARCH_DIR + '/pics.db'
+PART_OF_URL = 'http://walld.net/pics/'
+
+if os.path.exists(DB_FILE):
+    conn = sqlite3.connect(DB_FILE) # или :memory: чтобы сохранить в RAM
+    cursor = conn.cursor()
+else:
+    conn = sqlite3.connect(DB_FILE) # или :memory: чтобы сохранить в RAM
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE pics
+                  (id text, category text, sub_cat text, file_name text,
+                   resolution text, ratio text, url text)
+               """)
+    conn.commit()
+
+for filename in os.listdir(SEARCH_DIR):
+    print(filename)
+    sql = "SELECT * FROM pics WHERE file_name=?"
+    cursor.execute(sql, [(filename)])
+    ll = cursor.fetchone() # or use fetchone()
+    if ll == filename:
+        pass
+    else:
+        command = [('nope', 'abstract', 'nope', filename,\
+         'nope', 'nope', PART_OF_URL + filename)]
+        cursor.executemany("INSERT INTO pics VALUES (?, ?, ?, ?, ?, ?, ?)", command)
+conn.commit()
--- api.py
+++ api.py
@@ -1,11 +1,40 @@
 import flask
+from random import choice
+from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+def dict_factory(cursor, row): # need to make success line
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return '''<h1>Hi</h1>
+<p>This is a prototype API for sharing wallpapers.</p>'''
+
+@app.route('/apiv01/all', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('pics.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+    return jsonify(all_books)
+
+@app.route('/apiv01/random', methods=['GET'])
+def api_random():
+    conn = sqlite3.connect('pics.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM pics;').fetchall()
+    return jsonify(choice(all_books))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error':'404', 'success':'false'}), 404
 
 app.run()
