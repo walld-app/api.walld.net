45e6bc2bea7a7b6ee871d52a23a5c7cb9b37381d
kz159 <loh@kz159.ru>
2019-08-12 23:55:11 +0300

    added sync_del() function + cleanup

--- aku_aku.py
+++ aku_aku.py
@@ -1,7 +1,12 @@
+#This code fills db that api is looking for, providing with urls and
+#other stuff, kindly and jently.
+#For now (12.08.19) it can only write things like file name, category.
+#sub_category and url, need to add pillow maybe?
+
 #must run by cron every day to update db with wallpapers that sits in folders
-#and delete non-existing(404) ones
-#also rms old files
+
 import os, sqlite3
+
 SEARCH_DIR = '/mnt/ntfs-drive/walld_pics/'
 DB_FILE = SEARCH_DIR + 'pics.db'
 PART_OF_URL = 'http://walld.net/pics/'
@@ -9,19 +14,20 @@ PART_OF_URL = 'http://walld.net/pics/'
 TABLE_COLUMNS = """CREATE TABLE pics (id text, category text,
 sub_category text, file_name text, resolution text, ratio text, url text)"""
 
-'''checks if base exists, if not, creates one'''
+#checks if base exists, if not, creates one
 if os.path.exists(DB_FILE):
     conn = sqlite3.connect(DB_FILE)
-    conn.row_factory = lambda cursor, row: row[0]
+    conn.row_factory = sqlite3.Row#stack overflow code, learn it, dumbass
     cursor = conn.cursor()
     try:
         cursor.execute('SELECT * FROM pics')
     except sqlite3.OperationalError:
+        print('hmm')
         cursor.execute(TABLE_COLUMNS)
         conn.commit()
 else:
     conn = sqlite3.connect(DB_FILE)
-    conn.row_factory = lambda cursor, row: row[0]
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute(TABLE_COLUMNS)
     conn.commit()
@@ -31,58 +37,45 @@ def list_dir(dir):
     return subfolders
 
 def sync_add():
-    '''this code fills db that api is looking for, providing with urls and other stuff, kindly and jently
-    for now (07.08.19) it can only write things like file name and url, need to add pillow maybe?'''
-    for category in list_dir(SEARCH_DIR): # что если сделать из этого функцию и рекурсивно ходить по папкам? типа папки абстракт и искусство будут использованы названия папок в качестве темы
-        print('entering  category:' + category)
-        for sub_category in list_dir(SEARCH_DIR + category):
-            print('-'*30 + '>' + sub_category + '<' + '-'*30)
-            for filename in os.listdir(SEARCH_DIR + category + '/'+ sub_category):
-                sql = "SELECT file_name FROM pics WHERE file_name='"+filename+"'"
-                cursor.execute(sql)
-                ll = cursor.fetchone()
-                if ll == filename:
-                    pass
-                else:
-                    print(filename, 'is new here')
-                    command = [('nope', category, sub_category, filename,\
-                    'nope', 'nope', PART_OF_URL + category + '/' + sub_category + '/' + filename)]
-                    cursor.executemany("INSERT INTO pics VALUES (?, ?, ?, ?, ?, ?, ?)", command)
-    conn.commit()
-
-def sync_del():
-    #
-    #https://habr.com/en/post/321510/
-    '''This section emplements deleting non existing file.
-    if file was deleted for some reason, than we need to update our db'''
-    list = cursor.execute("SELECT * FROM pics").fetchall()
+    #что если сделать из этого функцию и
+    #рекурсивно ходить по папкам? типа папки абстракт и искусство будут
+    #использованы названия папок в качестве темы - ГОТОВО
     for category in list_dir(SEARCH_DIR):
         print('entering  category:' + category)
         for sub_category in list_dir(SEARCH_DIR + category):
             print('-'*30 + '>' + sub_category + '<' + '-'*30)
-            for filename in os.listdir(SEARCH_DIR + category + '/'+ sub_category):
-                script = """
-                SELECT * FROM pics WHERE category = '{}' \
-                AND sub_category = '{}' AND file_name = '{}'
-                """.format(category, sub_category, filename)
-                cursor.execute(script)
-                print(filename)
-                print('this is from sql: ', cursor.fetchall())
+            for filename in os.listdir(SEARCH_DIR +
+            category + '/'+ sub_category):
 
-        #else:
-        #    print('deleting', filename)
-        #    sql = "DELETE FROM pics WHERE file_name = ?"
-    #    cursor.execute(sql, [(filename)])
-    conn.commit()
+                sql = "SELECT file_name FROM pics WHERE \
+                file_name='{}'".format(filename)
+                cursor.execute(sql)
+                ll = cursor.fetchone()
+                if not ll:
+                    print(filename, 'is new here')
+                    command = [('id_here', category, sub_category, filename,\
+                    'resolution_here', 'ratio_here', PART_OF_URL + category + \
+                    '/' + sub_category + '/' + filename)]
+                    cursor.executemany("INSERT INTO pics \
+                    VALUES (?, ?, ?, ?, ?, ?, ?)", command)
+
+def sync_del():
+    print('*'*33, 'DELETE','*'*32)
+    '''This section emplements deleting non existing file.
+    if file was deleted for some reason, than we need to update our db'''
+    for i in cursor.execute('SELECT * FROM pics'):
+        file_path = SEARCH_DIR + i['category'] + \
+        '/' + i['sub_category'] + '/' + i['file_name']
+        if not os.path.exists(file_path):
+            print('deleting', file_path, 'from sql base')
+            sql = "DELETE FROM pics WHERE file_name = '{}'"\
+            .format(i['file_name'])
+            cursor.execute(sql)
 
 def main():
-    sync_del()
     sync_add()
+    sync_del()
     conn.commit()
+    conn.close()
 
 main()
-#Here comes part where it double checks thaat everything is accesseble,
-#i dont know,  maybe we dont need this part at all bc we have file check and aku_aku should trust web engine
-
-#list = cursor.execute("SELECT url FROM pics").cursor.fetchall()
-#for url in list:
