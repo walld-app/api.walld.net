f2727d938b6ad79144a4da2765ccdb58cac11a15
kz159 <loh@kz159.ru>
2019-09-10 15:42:35 +0300

    added config sample

---
+++ config_example.py
@@ -0,0 +1,4 @@
+SEARCH_DIR = 'THIS IS YOUR PICS DIRECTORY WHERE AKU AKU WILL TRY TO FIND images'
+DB_FILE = SEARCH_DIR[:-5] + 'pics.db' #this is pics db
+PART_OF_URL = 'https://walld.net/pics/' #this is where it will be stored on server 
+DEBUG = False #do you need pretty debug? set to true if so
