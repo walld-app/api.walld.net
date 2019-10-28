c8cad9d350d8dae4e8b28b0bcc6d99a07a11773b
kz159 <loh@kz159.ru>
2019-10-28 16:59:24 +0300

    done with postgresql support, now begining to test both sqlite3 and postgresql

--- aku_aku.py
+++ aku_aku.py
@@ -7,15 +7,13 @@ other stuff, kindly and jently.'''
 #must run by cron every day to update db with wallpapers that sits in folders
 
 import os
-import sqlite3
-import psycopg2
-import psycopg2.extras
 import multiprocessing
 from time import sleep
 import argparse
 import colorgram
 import requests
 from PIL import Image
+import sql_worker
 import config
 
 PARSER = argparse.ArgumentParser()
@@ -26,41 +24,12 @@ ARGS = PARSER.parse_args()
 MANAGER = multiprocessing.Manager()
 color_staff = MANAGER.dict()
 
-TABLE_COLUMNS = """CREATE TABLE pics (id int, category text,
-sub_category text, file_name text, width text, \
-height text, ratio text, color text, url text, locked int)""" #add locked state
-
-#checks if base exists, if not, creates one
 try:
     os.makedirs(config.SEARCH_DIR)
 except FileExistsError:
     pass
-if config.DB == 'postgres':
-    CONN = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER_NAME, 
-                        password=config.DB_PASSWORD, host=config.DB_HOST,
-                        port = '5159', cursor_factory=psycopg2.extras.DictCursor)
-    cursor = CONN.cursor()
-    cursor2 = CONN.cursor()
-    SQL = '%s'
-elif config.DB == 'sqlite3':
-    CONN = sqlite3.connect(config.DB_FILE)
-    CONN.row_factory = sqlite3.Row#stack overflow code, learn it, dumbass
-    cursor = CONN.cursor()
-    cursor2 = CONN.cursor() #second cursor is needed for deleting stuff
-    SQL = '?'
 
-else:
-    print('db is not recognized, use sqlite3 or postgres')
-    exit(1)
-    
-try:
-    cursor.execute('SELECT * FROM pics')
-except (sqlite3.OperationalError, psycopg2.ProgrammingError):
-    print('hmm')
-    if config.DB == 'postgres':
-        CONN.rollback()
-    cursor.execute(TABLE_COLUMNS)
-    CONN.commit()
+sql_boy = sql_worker.Sql_boy(db_type=config.DB)
 
 def list_dir(directory):
     '''returns generated list with folders'''
@@ -69,20 +38,16 @@ def list_dir(directory):
 
 def get_id():
     '''gets id based on existing maximun, returns -1 if didn`t find anything'''
-    try:
-        cursor.execute('SELECT MAX(id) FROM pics DESC LIMIT 1')
-    except psycopg2.ProgrammingError:
-        CONN.rollback()
+    result = sql_boy.execute('SELECT MAX(id) FROM pics DESC LIMIT 1', fetch='one')
+    if not result:
         return -1
-    row = cursor.fetchone()
-    if row[0]:
-        return row[0]
-    return -1
+    return result[0]
 
 def sync_add():
     '''recursivly walks on given folder and adds it
     to db using folder name as category'''
     idd = get_id()+1
+    print(idd)
     for category in list_dir(config.SEARCH_DIR):
         print('entering  category:' + category)
 
@@ -94,37 +59,39 @@ def sync_add():
                                        sub_category):
                 full_path = config.SEARCH_DIR + category + \
                             '/' + sub_category + '/' + filename
-                sql = "SELECT file_name FROM pics WHERE file_name={}".format(SQL)
-                cursor.execute(sql, (filename,))
-                found_row = cursor.fetchone()
+                if ' ' in full_path:
+                    os.rename(full_path, full_path.replace(' ', '_'))
+                    full_path = full_path.replace(' ', '_')
+                sql = sql_boy.gen_line("SELECT file_name FROM pics WHERE file_name={0}")
+                found_row = sql_boy.execute(sql, args=(filename,), fetch='one')
                 if not found_row:
                     print(filename, 'is new here')
-
                     with Image.open(full_path) as img:
                         width, height = img.size
-                    command = [(idd, category, sub_category, filename,
+                    command = (idd, category, sub_category, filename,
                                 width, height, 'ratio_here', 'no_color',
                                 config.PART_OF_URL + category + '/' + \
-                                sub_category + '/' + filename, '0')]
-                    cursor.executemany("INSERT INTO pics \
-                                        VALUES ({0}, {0}, {0}, {0}, \
-                                        {0}, {0}, {0}, {0}, {0}, {0})".format(SQL)
-                                        , command)
+                                sub_category + '/' + filename.replace(' ', '_'), '0')
+                    line = sql_boy.gen_line("INSERT INTO pics VALUES ({0}, {0}, {0},\
+                                             {0}, {0}, {0}, {0}, {0}, {0}, {0})")
+                    sql_boy.execute(line, command)
                     idd += 1
 
 def sync_del():
     '''This section emplements deleting non existing file.
     if file was deleted for some reason, than we need to update our db'''
     print('*'*33, 'DELETE', '*'*32)
+    sql_line = sql_boy.gen_line("SELECT * FROM pics WHERE url LIKE {0} ESCAPE ''")
+    list_of_boys = sql_boy.execute(sql_line, args=(config.PART_OF_URL,), fetch='all')
     try:
-        for row in cursor.execute('SELECT * FROM pics'):
+        for row in list_of_boys:
             file_path = config.SEARCH_DIR + row['category'] + \
             '/' + row['sub_category'] + '/' + row['file_name']
         
             if not os.path.exists(file_path):
                 print('deleting', file_path, 'from sql base')
-                sql = "DELETE FROM pics WHERE file_name = {}".format(SQL)
-                cursor2.execute(sql, (row['file_name'],))
+                sql = sql_boy.gen_line("DELETE FROM pics WHERE file_name = {0}")
+                sql_boy.execute(sql, args=(row['file_name'],))
     except TypeError:
         print('nothing to delete')
             # if we attempt to delete something on cursor
@@ -157,21 +124,22 @@ def main():
     ids = []
     sync_add()
     sync_del()
-    CONN.commit()
-    cursor.execute("SELECT * FROM pics WHERE color = 'no_color' AND locked = '0'")
-    sql = "UPDATE pics SET color = {0} WHERE id = {0}".format(SQL) #add LOCKED state
+    sql_boy.commit()
+    row_list = sql_boy.execute("SELECT * FROM pics WHERE color = 'no_color' AND locked = '0'", fetch='all')
+    sql = sql_boy.gen_line("UPDATE pics SET color = {0} WHERE id = {0}")
 
     if ARGS.c:
         for _ in range(ARGS.c):
-            row = cursor.fetchone()
+            row = row_list.pop(0)
             if row:
-                cursor2.execute("UPDATE pics SET locked = '1' WHERE id = '{}'".format(row['id']))
-                CONN.commit()
+                sql_boy.execute(sql_boy.gen_line("UPDATE pics SET locked = '1' WHERE id = {0}"), (row['id'], ))
                 ids.append(row['id'])
                 thread = multiprocessing.Process(target=calc_colors,
                                              args=(row,))
                 procs.append(thread)
                 thread.start()
+        print('aha')
+        sql_boy.commit()
         while alive:
             get = []
             for i in procs:
@@ -186,17 +154,17 @@ def main():
 
     elif ARGS.n:
         for _ in range(ARGS.n):
-            row = cursor.fetchone()
-            cursor.execute("UPDATE pics SET locked = '1' WHERE id = '{}'".format(row['id']))
-            CONN.commit()
+            row = row_list(0)
+            sql_boy.execute(sql_boy.gen_line("UPDATE pics SET locked = '1' WHERE id = {0}"), (i, ))
+            sql_boy.commit()
             calc_colors(row)
             ids.append(row['id'])
 
     for i in ids:
-        cursor.execute("UPDATE pics SET locked = '0' WHERE id = '{}'".format(i))
+        sql_boy.execute(sql_boy.gen_line("UPDATE pics SET locked = '0' WHERE id = {0}"), (i, ))
     for key in color_staff:
-        cursor.execute(sql, (color_staff[key], key))
+        sql_boy.execute(sql, (color_staff[key], key))
 
-    CONN.commit()
-    CONN.close()
+    sql_boy.commit()
+    sql_boy.close_connection()
 main()
