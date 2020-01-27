3ed6e086d6e23800df8ddd1c2cb6a441b2de482a
kz159 <loh@kz159.ru>
2020-01-28 00:10:05 +0300

    go out from folder

--- api.py
+++ api.py
@@ -1,77 +1,11 @@
-#!/usr/bin/python3
-'''api implementation in python'''
-from random import choice
-import flask
-from flask import request
-import sql_worker
-import config
+'''main launcher of api'''
+from aiohttp import web
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = config.DEBUG
-__version__ = '02'
-sql_boy = sql_worker.SqlBoy(db_type=config.DB)
+from views import healthcheck, get_info
+from config import VERSION 
 
-@app.route('/', methods=['GET'])
-def home():
-    '''returns "home" page'''
-    return '''<h1>Hi</h1>
-<p>This is a prototype API for sharing wallpapers.</p>'''
+APP = web.Application()
+APP.router.add_get('/', healthcheck)
+APP.router.add_get(f'/api/{VERSION}', get_info)
 
-@app.route('/apiv01/', methods=['GET'])
-def api_version():
-    '''some info about api and other stored here'''
-    param = request.args.get('param')# listens ?param=
-    if param == 'categories':
-        query = "SELECT DISTINCT category FROM pics"
-        result = sql_boy.execute(query, fetch='all')
-        for category in result:
-            query = sql_boy.gen_line("SELECT DISTINCT sub_category \
-                                       FROM pics WHERE category = {0}")
-            sub_categories = sql_boy.execute(query, args=(category['category'], ), fetch='all')
-            category['subs'] = [] # refactor
-            for sub_category in sub_categories:
-                category['subs'].append(sub_category['sub_category'])
-        return flask.jsonify({'success':True, 'content': result})
-
-    if param == 'version':
-        return flask.jsonify({'success':True, 'content': {'version' : __version__}})
-    return page_not_found(404)
-
-@app.route('/apiv01/walls')
-def deliver_walls():
-    '''main route for accessing walls'''
-    category = request.args.getlist('category')
-    sub_category = request.args.getlist('sub_category')
-    random = request.args.getlist('random')
-    query = "SELECT * FROM pics WHERE"
-    to_filter = []
-    if random:
-        all_walls = sql_boy.execute('SELECT * FROM pics', fetch='all')
-        return flask.jsonify({'success':True, 'content':choice(all_walls)})
-    if category:
-        for i in category:
-            print('going to cycle', i)
-            query += ' category={0}'
-            to_filter.append(i)
-    if sub_category:
-        query += ' AND'
-        for i in sub_category:
-            print('some subcategories', i)
-            query += ' sub_category={0}'
-            to_filter.append(i)
-    if not (category or random):
-        return page_not_found(404)
-    query = sql_boy.gen_line(query)
-    print(query, '< query')
-    result = sql_boy.execute(query, args=to_filter, fetch='all')
-    if result:
-        return flask.jsonify({'success':True, 'content':choice(result)}) #nosec
-    return page_not_found(404)
-
-@app.errorhandler(404)
-def page_not_found(error_code):
-    '''404 route'''
-    return flask.jsonify({'error':'404', 'success':False}), 404
-
-app.run()
-sql_boy.close_connection()
+web.run_app(APP)
--- sql_worker.py
+++ sql_worker.py
@@ -1,88 +1,19 @@
-'''postgresql and sqlite3 lib for DRY code'''
-import sqlite3
-import sys
-import psycopg2
-import psycopg2.extras
-import config
+''' sql worker module that helps with quering'''
 
-class SqlBoy():
-    '''class represents connection for postgre and(in future) sqlite3'''
-    def __init__(self, db_type=None):
-        self.db_type = db_type
-        self.table_columns = """CREATE TABLE pics (id int, category text, \
-                            sub_category text, file_name text, width text, \
-                            height text, ratio text, color text, url text, locked int)"""
-        if self.db_type == 'postgres':
-            self.conn = psycopg2.connect(
-                dbname=config.DB_NAME, user=config.DB_USER_NAME,
-                password=config.DB_PASSWORD, host=config.DB_HOST,
-                port=config.DB_PORT, cursor_factory=psycopg2.extras.RealDictCursor
-            )
-            self.cursor = self.conn.cursor()
-            self.cursor2 = self.conn.cursor()
-            self.sql = '%s'
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_structure import BASE
+from helpers import get_connection_dsn
 
-        elif self.db_type == 'sqlite3':
-            self.conn = sqlite3.connect(config.DB_FILE, check_same_thread=False)
-            self.conn.row_factory = sqlite3.Row#stack overflow code, learn it, dumbass
-            self.cursor = self.conn.cursor()
-            self.cursor2 = self.conn.cursor()
-            self.sql = '?'
+ENGINE = create_engine(get_connection_dsn())
+BASE.metadata.create_all(ENGINE) # REDO TO ALEMBIC 
+SESSION = sessionmaker(bind=ENGINE)
 
-        elif not self.db_type:
-            print('you didn`t specify db name in config!')
-            sys.exit(1)
-
-        try:
-            self.cursor.execute('SELECT * FROM pics')
-        except (sqlite3.OperationalError, psycopg2.ProgrammingError):
-            if self.db_type == 'postgres':
-                self.conn.rollback()
-            self.cursor.execute(self.table_columns)
-            self.conn.commit()
-
-    def execute(self, line, args=None, fetch='all'):
-        '''executes line with or without args'''
-        line += ';'
-        row_dicts = {}
-        try:
-            self.cursor.execute(line, args)
-        except psycopg2.ProgrammingError:
-            self.conn.rollback()
-            return row_dicts
-        result = self.cursor.execute(line, args)
-        if fetch == 'all':
-            if self.db_type == 'postgres':
-                list_of_dicts = []
-                try:
-                    fetchall = self.cursor.fetchall()
-                except psycopg2.ProgrammingError:
-                    return row_dicts
-                for row in fetchall:#NEED NEW FUNCTION AND
-                    for content in row:#POSSIBLY CONTRIBUTE THIS
-                        row_dicts[content] = row[content]#TO PSYCOPG2, AWFUL
-                    list_of_dicts.append(row_dicts)
-                    row_dicts = {}
-                return list_of_dicts
-
-        elif fetch == 'one':
-            if self.db_type == 'postgres':
-                list_of_lines = self.cursor.fetchone()
-                if not list_of_lines:
-                    return row_dicts
-                for i in list_of_lines:
-                    row_dicts[i] = list_of_lines[i]
-                return row_dicts
-            return result.fetchone()
-
-    def gen_line(self, line):
-        '''gens line based on sql format'''
-        return line.format(self.sql)
-
-    def commit(self):
-        '''commits changes'''
-        self.conn.commit()
-
-    def close_connection(self):
-        '''closes connection'''
-        self.conn.close()
+def get_distinct(target):
+    '''gets distinct values by CLASS.COLUMN'''
+    try:
+        session = SESSION()
+        distincts = [i[0] for i in session.query(target).distinct()] #КОСТЫЛЬ ПЕРЕДЕЛАТЬ
+        return distincts
+    except :# докинуть ошибку
+        print('error')
